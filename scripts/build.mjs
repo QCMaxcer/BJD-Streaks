073928ec7009c4a795c5af267a1cfa616ea60648
QC_Max <2025763418@qq.com>
2026-06-13 01:58:32 +0800

    简单调整

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -4,7 +4,7 @@ import { build } from "esbuild";
 const banner = `// ==UserScript==
 // @name         布吉岛战绩与连胜统计
 // @namespace    https://user.mcbjd.net/
-// @version      1.2.2
+// @version      1.0.0
 // @description  分类查找布吉岛战绩，并统计当前连胜与历史最高连胜
 // @author       Codex
 // @match        https://user.mcbjd.net/*
